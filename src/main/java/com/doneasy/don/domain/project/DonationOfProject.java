c08e0562f98bbaf21aec0583b957d861ba6e7675
salbyul <kpast96@gmail.com>
2022-12-30 15:06:09 +0900

    init

---
+++ src/main/java/com/doneasy/don/domain/project/DonationOfProject.java
@@ -0,0 +1,12 @@
+package com.doneasy.don.domain.project;
+
+import java.time.LocalDateTime;
+
+public class DonationOfProject {
+
+    private Long id;
+    private Integer price;
+    private LocalDateTime createdDate;
+    private Long memberId;
+    private Long projectId;
+}
---
+++ src/main/java/com/doneasy/don/domain/project/SupportOfProject.java
@@ -0,0 +1,11 @@
+package com.doneasy.don.domain.project;
+
+import java.time.LocalDateTime;
+
+public class SupportOfProject {
+
+    private Long id;
+    private LocalDateTime createdDate;
+    private Long memberId;
+    private Long projectId;
+}
